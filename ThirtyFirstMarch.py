862e47ca90032681c9b92cee068bf36a5ee8f6d8
PradeepPadmanaban <30297503+PradeepPadmanaban@users.noreply.github.com>
2018-01-02 16:36:29 +0530

    Delete ThirtyFirstMarch.py

--- ThirtyFirstMarch.py
+++
@@ -1,5 +0,0 @@
-print('Hello There (Single Quote)!')
-print("Hello There (Double Quote)!")
-print("""This is multiple line
-string using triple double quotes.
-you can also use triple single quotes.""")
